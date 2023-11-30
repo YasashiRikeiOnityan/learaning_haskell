ca040b78b95b2a2c17013fbbf81ba1b330db477a
YasashiRikeiOnityan <nanshanlu524@gmail.com>
2023-11-30 19:49:26 +0900

    add recursively defined function factorial'

--- learn_you_a_haskell_for_great_good/chapter2/integer.hs
+++ learn_you_a_haskell_for_great_good/chapter2/integer.hs
@@ -1,5 +1,9 @@
 factorial :: Integer -> Integer
 factorial n = product [1..n]
 
+factorial' :: Integer -> Integer
+factorial' 0 = 1
+factorial' n = n * factorial (n - 1)
+
 big :: Integer
 big = factorial 50
