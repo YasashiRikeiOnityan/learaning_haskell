7382e4866720e45f1f1b6496416f46ef960eb675
YasashiRikeiOnityan <nanshanlu524@gmail.com>
2023-12-19 19:33:28 +0900

    edit documentation

--- atcoder/competitive_programming_rules/chapter1/A06.hs
+++ atcoder/competitive_programming_rules/chapter1/A06.hs
@@ -1,20 +1,36 @@
-import Control.Monad ( replicateM )
+{-
+import Control.Monad ( replicateM_ )
 
 cumulativeSum :: [Int] -> [Int]
 cumulativeSum = scanl (+) 0
 
-calc :: [Int] -> [Int] -> Int
-calc s [l, r] = (s !! r) - (s !! (l - 1))
-
-makeOutput :: [Int] -> String
-makeOutput [] = ""
-makeOutput (x : xs) = show x ++ "\n" ++ makeOutput xs
+solve :: [Int] -> [Int] -> Int
+solve s [l, r] = (s !! r) - (s !! (l - 1))
 
 main :: IO ()
 main = do
     [_, q] <- map read . words <$> getLine :: IO [Int]
     as <- map read . words <$> getLine :: IO [Int]
-    lr <- replicateM q getLine :: IO [String]
-    let lr_ = map words lr
-    let lrs = map (\m -> map read m :: [Int]) lr_
-    putStrLn $ makeOutput $ map (calc (cumulativeSum as)) lrs
+    let cumSum = cumulativeSum as
+    replicateM_ q $ do
+        lr <- map read . words <$> getLine :: IO [Int]
+        print $ solve cumSum lr
+-}
+
+import Control.Monad ( replicateM_ )
+import Data.Array ( Array, (!), elems, listArray )
+
+cumulativeSum :: Int -> Int -> Array Int Int -> Array Int Int
+cumulativeSum l r as = listArray (l, r) $ scanl (+) 0 $ elems as
+
+solve :: Array Int Int -> [Int] -> Int
+solve s [l, r] = (s ! r) - (s ! (l - 1))
+
+main :: IO ()
+main = do
+    [n, q] <- map read . words <$> getLine :: IO [Int]
+    as <- listArray (0, n - 1) . map read . words <$> getLine :: IO (Array Int Int)
+    let cumSum = cumulativeSum 0 n as
+    replicateM_ q $ do
+        lr <- map read . words <$> getLine :: IO [Int]
+        print $ solve cumSum lr
