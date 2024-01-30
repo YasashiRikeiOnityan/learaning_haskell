3662795a06a44556fc91fe073cb212b545da18a5
minamiyama <ri-minamiyama@kumahira-safe.co.jp>
2024-01-30 15:33:09 +0900

    A07のプログラムを更新

--- atcoder/competitive_programming_rules/chapter2/A07.hs
+++ atcoder/competitive_programming_rules/chapter2/A07.hs
@@ -1,15 +1,17 @@
-import Control.Monad
-import Data.Array
+import Control.Monad ( replicateM )
+import Data.Array ( accumArray, elems, Array )
 
-cumulativeSum :: (Int, Int) -> Array Int Int -> Array Int Int
-cumulativeSum lr xs = listArray lr $ scanl (+) 0 $ elems xs
+add2PreviousDay :: [Int] -> [(Int, Int)]
+add2PreviousDay [l, r] = [(l, 1), (r + 1, -1)]
+add2PreviousDay _ = []
 
-solve :: (Int, Int) -> Array Int Int -> Int
-solve (l, r) as = as ! r - as ! (l - 1)
+cumulativeSum :: Array Int Int -> [Int]
+cumulativeSum xs = scanl (+) 0 $ elems xs
 
 main :: IO ()
 main = do
     d <- readLn :: IO Int
     n <- readLn :: IO Int
-    lrs <- map (map read . words) <$> replicateM n getLine :: IO [[Int]]
-    print lrs
+    lrs <- concatMap (add2PreviousDay . fmap read . words) <$> replicateM n getLine :: IO [(Int, Int)]
+    let accArray = accumArray (+) 0 (1, d + 1) lrs
+    mapM_ print $ tail $ init $ cumulativeSum accArray
