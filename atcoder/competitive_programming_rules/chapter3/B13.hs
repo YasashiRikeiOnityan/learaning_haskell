744fed18cff6ae1360726cd8885bef22bcd405d4
minamiyama <ri-minamiyama@kumahira-safe.co.jp>
2024-02-13 16:04:34 +0900

    しゃくとり法！完成！

--- atcoder/competitive_programming_rules/chapter3/B13.hs
+++ atcoder/competitive_programming_rules/chapter3/B13.hs
@@ -5,20 +5,20 @@ import Data.ByteString ( ByteString )
 import Data.Maybe ( fromJust )
 import Data.Array ( Array, listArray, (!) )
 
-step :: Int -> Int -> (Int, Int) -> Int -> Array Int Int -> Int -> (Int, Int)
-step l r (start, sum_) price arr k
-    | start < r && price_ <= k = step l r (start + 1, sum_) price_ arr k
-    | start == r && price_ <= k = (start, sum_ + start - l + 1)
-    | otherwise = (start, sum_ + start - l)
+step :: Int -> Int -> (Int, Int, Int) -> Array Int Int -> Int -> (Int, Int, Int)
+step l r (start, price, sum_) arr k
+    | start <= r && price_ <= k = step l r (start + 1, price_, sum_) arr k
+    | otherwise = (start, price__, sum_ + start - l)
     where
         price_ = price + arr ! start
+        price__ = price_ - (arr ! l) - (arr ! start)
 
-syakutori :: Int -> Int -> Int -> Array Int Int -> Int -> Int -> Int 
-syakutori l r start arr k sum_
-    | l == r = if arr ! r <= k then sum_ + 1 else sum_
-    | otherwise = syakutori (l + 1) r newStart arr k newSum
+syakutori :: Int -> Int -> Int -> Array Int Int -> Int -> Int -> Int -> Int 
+syakutori l r start arr k price sum_
+    | l > r = sum_
+    | otherwise = syakutori (l + 1) r newStart arr k newPrice newSum
         where
-            (newStart, newSum) = step l r (start, sum_) 0 arr k
+            (newStart, newPrice, newSum) = step l r (start, price, sum_) arr k
 
 main :: IO ()
 main = do
@@ -26,9 +26,11 @@ main = do
     as <- map readInt . BS.words <$> BS.getLine
     let left = 1
         right = n
+        start = left
         arr = listArray (left, right) as
+        price = 0
         sum_ = 0
-    print $ syakutori left right left arr k sum_
+    print $ syakutori left right start arr k price sum_
 
 readInt :: ByteString -> Int
 readInt = fst . fromJust . BS.readInt
