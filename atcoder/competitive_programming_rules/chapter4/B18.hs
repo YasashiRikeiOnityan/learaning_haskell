0894000228755ae0033e58e671b6a2b2f6ce7314
YasashiRikeiOnityan <nanshanlu524@gmail.com>
2024-06-11 07:23:52 +0900

    B18 TLE

--- atcoder/competitive_programming_rules/chapter4/B18.hs
+++ atcoder/competitive_programming_rules/chapter4/B18.hs
@@ -5,27 +5,41 @@ import Data.ByteString ( ByteString )
 import Data.Maybe ( fromJust )
 import Data.Bifunctor ( bimap )
 
-judge :: Int -> [(Int, Int)] -> [(Int, [Int])] -> [(Int, [Int])]
-judge _ [] bs = bs
-judge s (a:as) bs = judge s as (partialMap s a bs)
+judge :: Int -> [(Int, Int)] -> [(Int, [Int])] -> (Int, [Int])
+judge _ [] _ = (-1, [])
+judge s (a : as) bs = if fst checked == s then checked else judge s as bs'
+    where
+        bs' = partialMap s a bs
+        checked = check s bs'
+
+f :: Int -> [(Int, Int)] -> [(Int, [Int])] -> [(Int, [Int])]
+f _ [] bs = bs
+f s (a : as) bs = f s as bs'
+    where
+        bs' = partialMap s a bs
 
 partialMap :: Int -> (Int, Int) -> [(Int, [Int])] -> [(Int, [Int])]
 partialMap _ _ [] = []
-partialMap s x (y:ys)
+partialMap s x (y : ys)
     | snd x + fst y > s = y : partialMap s x ys
     | snd x + fst y == s = [(s, fst x : snd y)]
     | otherwise = y : bimap (snd x +) (fst x :) y : partialMap s x ys
 
+check :: Int -> [(Int, [Int])] -> (Int, [Int])
+check _ [] = (-1, [])
+check s (y : ys)
+    | fst y == s = y
+    | otherwise = check s ys
+
 b18 :: Int -> [(Int, Int)] -> String
 b18 s as = if fst res == s then (show . length . snd $ res) ++ "\n" ++ (unwords . map show . reverse $ snd res) else "-1"
-    where res = last $ judge s as [(0, [])]
+    where res = judge s as [(0, [])]
 
 main :: IO ()
 main = do
     [_, s] <- map readInt . BS.words <$> BS.getLine
     as <- map readInt . BS.words <$> BS.getLine
-    print . length $ judge s (zip [1..] as) [(0, [])]
-    --putStrLn $ b18 s (zip [1..] as)
+    putStrLn $ b18 s (zip [1..] as)
 
 readInt :: ByteString -> Int
 readInt = fst . fromJust . BS.readInt
