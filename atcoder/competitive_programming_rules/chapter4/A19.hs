df90fd8247192088ee66bfa6254a2b281173b472
minamiyama <ri-minamiyama@kumahira-safe.co.jp>
2024-06-13 16:50:53 +0900

    速い！ナップザック問題

--- atcoder/competitive_programming_rules/chapter4/A19.hs
+++ atcoder/competitive_programming_rules/chapter4/A19.hs
@@ -4,17 +4,20 @@ import Control.Monad ( replicateM )
 import Data.Bifunctor ( bimap )
 
 knapsack :: Int -> [(Int, Int)] -> [(Int, Int)] -> Int
-knapsack _ [] vs = maximum_ 0 vs
-knapsack c (wv : wvs) vs = knapsack c wvs (partialMap c wv vs)
+knapsack _ [] vs = snd . head $ vs
+knapsack c ((w, v) : wvs) vs = knapsack c wvs . merge vs . filter ((<= c) . fst) . map (bimap (w +) (v +)) $ vs
 
-partialMap :: Int -> (Int, Int) -> [(Int, Int)] -> [(Int, Int)]
-partialMap _ (_, _) [] = []
-partialMap c (w, v) (x : xs)
-    | w + fst x > c = x : partialMap c (w, v) xs
-    | otherwise = x : bimap (w +) (v +) x : partialMap c (w, v) xs
-
-maximum_ :: Int -> [(Int, Int)] -> Int
-maximum_ = foldl (\ m x -> max m (snd x))
+merge :: [(Int, Int)] -> [(Int, Int)] -> [(Int, Int)]
+merge [] wvs = wvs
+merge wvs [] = wvs
+merge wvs1@((w1, v1) : wv1) wvs2@((w2, v2) : wv2) = case compare w1 w2 of
+    LT -> case compare v1 v2 of
+        LT -> (w2, v2) : merge wvs1 wv2
+        _  -> merge wvs1 wv2
+    EQ -> (w1, max v1 v2) : merge wv1 wv2
+    GT -> case compare v1 v2 of
+        GT -> (w1, v1) : merge wv1 wvs2
+        _  -> merge wv1 wvs2
 
 toPair :: [Int] -> (Int, Int)
 toPair [x, y] = (x, y)
@@ -22,41 +25,6 @@ toPair _ = (0, 0)
 
 main :: IO ()
 main = do
-    [n, w] <- map read . words <$> getLine :: IO [Int]
-    wvs <- map (toPair . map read . words) <$> replicateM n getLine :: IO [(Int, Int)]
+    [n, w] <- map read . words <$> getLine
+    wvs <- map (toPair . map read . words) <$> replicateM n getLine
     print $ knapsack w wvs [(0, 0)]
-
-{-
-4 7
-3 13
-3 17
-5 29
-1 10
-
-40
--}
-
-{-
-type Capacity = Int
-type Value = Int
-type Weight = Int
-data Item = Item { weight :: Weight, value :: Value } deriving Show
-data KSProblem = KSP { capacity :: Capacity, items :: [Item]}
-
-knapsack :: KSProblem -> Value
-knapsack (KSP _ []) = 0
-knapsack (KSP c (i : is))
-    | weight i <= c = max (knapsack (KSP c is))
-                          (value i + knapsack (KSP (c - weight i) is))
-    | otherwise = knapsack (KSP c is)
-
-toItem :: [Int] -> Item
-toItem [x, y] = Item x y
-toItem _ = Item 0 0
-
-main :: IO ()
-main = do
-    [n, w] <- map read . words <$> getLine :: IO [Int]
-    wvs <- map (toItem . map read . words) <$> replicateM n getLine
-    print 0
--}
